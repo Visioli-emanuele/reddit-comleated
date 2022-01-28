bd067d7ee2c9f233dca0e68a09e4cd3c66fb79a5
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-01-28 12:59:27 +0000

    putno 2.1

--- reddit-completo/src/app/article/article.component.ts
+++ reddit-completo/src/app/article/article.component.ts
@@ -1,28 +1,22 @@
-import { Component, OnInit, HostBinding} from '@angular/core';
+import { Component, OnInit, HostBinding} from'@angular/core';
 import { Article } from './ article.model';
 @Component({
   selector: 'app-article', //Il tag per inserire il componente sarà <app-article>
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
-  @HostBinding('attr.class') cssClass = 'card';
-  votes: number;
-  title: string;
-  link: string;
-
-  constructor() {
-    this.title = ' Angular 2 ';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+export class AppComponent {
+  articles:Article[];   // <-- component property
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
   }
-  voteUp():Boolean { //Aggiunto tipo di ritorno
-    this.votes += 1;
-    return false; //Non propagare l'evento 
-}
-voteDown():Boolean{
-    this.votes -= 1;
-    return false; //Non propagare l'evento 
-}
-  ngOnInit() {}
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article('Angular 2', 'http://angular.io', 3));
+    return false;
+  }
 }
